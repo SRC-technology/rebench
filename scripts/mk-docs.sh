27ac3ad8b79fdb41a733c5fcc2502dfcab96651f
Leandro Ostera <leandro@ostera.io>
2018-09-20 10:10:37 +0200

    +[docs] First stab at docs

---
+++ scripts/mk-docs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+readonly PKG=ReBench
+
+readonly DOCS=docs
+readonly ODOC=$(which odoc)
+readonly LIB=./lib/bs/src
+
+readonly CMT_FILES=$(find ${LIB} -name "*.cmti")
+readonly ODOC_FILES=$(echo ${CMT_FILES} | sed "s/cmti/odoc/g")
+
+echo "<< Compiling docs..."
+for file in ${CMT_FILES}; do
+  ${ODOC} compile \
+    -I ${LIB} \
+    --pkg=${PKG} \
+    ${file}
+done
+echo ">> Done!"
+
+echo "<< Generating HTML..."
+for file in ${ODOC_FILES}; do
+  ${ODOC} html \
+    -I ${LIB} \
+    -o ${DOCS} \
+    --syntax=re \
+    --semantic-uris \
+    ${file}
+done
+echo ">> Done!"
